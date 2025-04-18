a9657a4d83b739d534b923c280c13e501521a161
Anirban Banerjee <anirbanbanerjee050@gmail.com>
2025-04-19 03:10:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-chmod +x fix-imports.sh
-[ -f vercel-build.sh ] && chmod +x vercel-build.sh
+chmod +x fix-imports.sh && ./fix-imports.sh
 rm -rf .next out
-./fix-imports.sh
-find . -name ".babelrc*" -o -name "babel*" -not -path "*/node_modules/*" -exec rm -f {} \; 2>/dev/null || true
-export NODE_PATH=.
-export DISABLE_ESLINT_PLUGIN=true
-export NEXT_DISABLE_ESLINT=1
-export SWC_MINIFY=true
-export NEXT_TELEMETRY_DISABLED=1
-export NEXT_DISABLE_ETW=1
-export NODE_OPTIONS=--max_old_space_size=4096
+echo "{}" > .babelrc
+export NODE_PATH=. DISABLE_ESLINT_PLUGIN=true NEXT_DISABLE_ESLINT=1 SWC_MINIFY=true 
 next build --no-lint
 exit 0 
